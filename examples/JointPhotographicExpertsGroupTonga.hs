e49bd142d7c22680527c27f5c43818fe1ce83d6a
Hao Lian <me@haolian.org>
2014-07-27 01:03:32 -0400

    JointPhotographicExpertsGroupTonga.hs: example

--- examples/JointPhotographicExpertsGroupTonga.hs
+++ examples/JointPhotographicExpertsGroupTonga.hs
@@ -1,63 +1,36 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, NoImplicitPrelude #-}
 
 module JointPhotographicExpertsGroupTonga where
 
-import           Control.Applicative
-import           Control.Lens hiding ((.=))
-import           Control.Monad
-import           Data.Attoparsec.ByteString
-import qualified Data.Attoparsec.ByteString as P
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Lazy as L
-import qualified Data.Text.Format as F
-import           Data.Text.Lazy (Text)
-import qualified Data.Text.Lazy as T
-import           Data.Text.Lazy.Encoding (decodeUtf8)
-import           Data.Text.Lazy.IO
-import           Data.Word8
-import           Network.HTTP.Types (status200)
-import           Network.Linklater
-import           Network.Wai
-import           Network.Wai.Handler.Warp (run)
-import           Network.Wreq hiding (params, headers)
-import           Prelude hiding (readFile, writeFile, putStrLn)
+import BasePrelude hiding (words, intercalate)
+import Control.Lens ((^.))
+import Data.Attoparsec.Text.Lazy
+import Data.Text.Lazy hiding (filter)
+import Data.Text.Lazy.Encoding (decodeUtf8)
+import Network.HTTP.Types (status200)
+import Network.Linklater (say, slash, Channel(..), Command(..), User(..), Config(..), Message(..), Icon(..))
+import Network.Wai (Application, responseLBS)
+import Network.Wai.Handler.Warp (run)
+import Network.Wreq hiding (params)
 
-toText :: B.ByteString -> Text
-toText = decodeUtf8 . L.fromChunks . return
+findUrl :: Text -> Maybe Text
+findUrl = fmap fromStrict . maybeResult . parse (manyTill (notChar '\n') (string "src=\"") *> takeTill (== '"'))
 
-token :: Text
-token = undefined
-
-urlParser :: Parser B.ByteString
-urlParser = p
-  where
-    p = garbage *> url
-    garbage = string "src=\"" <|> (P.take 1 *> garbage)
-    url = takeTill (== _quotedbl)
-
-urlFor :: Text -> IO Text
-urlFor search = do
-  r <- get (T.unpack $ F.format "http://{}.jpg.to/" [search])
-  (return . handle . parse urlParser . strictly) (r ^. responseBody)
-  where
-    strictly = B.concat . L.toChunks
-    handle (Fail i ctxs s) = error (show (i, ctxs, s))
-    handle (Partial f) = handle (f "")
-    handle (Done _ r) = toText r
+messageOf :: User -> Channel -> Text -> Text -> Message
+messageOf (User u) c search = Message (EmojiIcon "gift") c . mappend (mconcat ["@", u, " Hello, wanderer. I found you this for \"", search, "\": "])
 
 jpgto :: Maybe Command -> Application
-jpgto (Just (Command (User user) channel text)) req respond = do
-  url <- urlFor (maybe "spolsky" id text)
-  say (Message channel (response url) (EmojiIcon "gift")) config
-  (respond . responseLBS status200 headers) ""
-  where
-    response url = F.format "@{} {}" (user, url)
-    config = Config token "trello.slack.com"
-    headers = [("Content-Type", "text/plain")]
+jpgto (Just (Command user channel (Just text))) _ respond = do
+  message <- get url >>= (return . fmap (messageOf user channel text) . findUrl . decodeUtf8 . flip (^.) responseBody)
+  case (debug, message) of
+    (True, _) -> putStrLn ("+ Pretending to post " <> show message) >> respondWith ""
+    (False, Just m) -> config' >>= say m >> respondWith ""
+    (False, Nothing) -> respondWith "Something went wrong!"
+  where ourHeaders = [("Content-Type", "text/plain")]
+        respondWith = respond . responseLBS status200 ourHeaders
+        config' = (Config "trello.slack.com" . pack . filter (/= '\n')) <$> readFile "token"
+        url = "http://" <> (unpack . intercalate "." . words $ text) <> ".jpg.to/"
+        debug = True
 
 main :: IO ()
-main = do
-  let port = 80
-  putStrLn (F.format "+ Listening on port {}" [port])
-  run port (slash jpgto)
-  return ()
+main = let port = 3000 in putStrLn ("+ Listening on port " <> show port) >> run port (slash jpgto)
