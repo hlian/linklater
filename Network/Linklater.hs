a055a4435cc67b8d50f52b2a85e832e7c7218a99
Hao Lian <me@haolian.org>
2014-07-27 01:51:08 -0400

    user friendliness: improved! better docs, and `slashSimple`

--- Network/Linklater.hs
+++ Network/Linklater.hs
@@ -3,57 +3,36 @@
 -- |
 -- Module: Network.Linklater
 -- Copyright: (c) The Linklaterteers
---
 -- License: BSD-style
+-- Maintainer: me@haolian.org
 -- Stability: experimental
 -- Portability: GHC
---
--- Features:
---
--- * Uses @Text@ everywhere so you can send your slash commands crazy Unicode characters all day long.
--- * Lovely documentation.
--- * Battle-tested.
 --
 -- Here's a @/jpgto@ bot! If you run this program and then tell Slack
 -- about your server (incoming hook and custom slash command) and then
 -- type @/jpgto diplomatico@ in one of your channels, you'll get the
 -- image from @http://diplomatico.jpg.to@. How, you say? /Screen scraping/.
 --
--- @
--- urlParser :: Parser B.ByteString
--- urlParser = p
---   where
---     p = garbage *> url
---     garbage = string "src=\"" <|> (P.take 1 *> garbage)
---     url = takeTill (== _quotedbl)
+-- > findUrl :: Text -> Maybe Text
+-- > findUrl = fmap fromStrict . maybeResult . parse (manyTill (notChar '\n') (string "src=\"") *> takeTill (== '"'))
 --
--- urlFor :: Text -> IO Text
--- urlFor search = do
---   r <- get (T.unpack $ F.format "http://{}.jpg.to/" [search])
---   (return . handle . parse urlParser . strictly) (r ^. responseBody)
---   where
---     strictly = B.concat . L.toChunks
---     handle (Fail i ctxs s) = error (show (i, ctxs, s))
---     handle (Partial f) = handle (f "")
---     handle (Done _ r) = toText r
+-- > messageOf :: User -> Channel -> Text -> Text -> Message
+-- > messageOf (User u) c search = Message (EmojiIcon "gift") c . mappend (mconcat ["@", u, " Hello, wanderer. I found you this for \"", search, "\": "])
 --
--- jpgto :: Maybe Command -> Application
--- jpgto (Just (Command (User user) channel text)) req respond = do
---   url <- urlFor (maybe "spolsky" id text)
---   say (Message channel (response url) (EmojiIcon "gift")) config
---   (respond . responseLBS status200 headers) ""
---   where
---     response url = F.format "@{} {}" (user, url)
---     config = Config token "trello.slack.com"
---     headers = [("Content-Type", "text/plain")]
+-- > jpgto :: Maybe Command -> IO Text
+-- > jpgto (Just (Command user channel (Just text))) = do
+-- >   message <- (fmap (messageOf user channel text) . findUrl . decodeUtf8 . flip (^.) responseBody) <$> get url
+-- >   case (debug, message) of
+-- >     (True, _) -> putStrLn ("+ Pretending to post " <> show message) >> return ""
+-- >     (False, Just m) -> config' >>= say m >> return ""
+-- >     (False, Nothing) -> return "Something went wrong!"
+-- >   where config' = (Config "trello.slack.com" . pack . filter (/= '\n')) <$> readFile "token"
+-- >         url = "http://" <> (unpack . intercalate "." . words $ text) <> ".jpg.to/"
+-- >         debug = True
+-- > jpgto Nothing = return "Type more! (Did you know? jpgtobot is only 26 lines of Haskell. <https://github.com/hlian/jpgtobot/blob/master/Main.hs>)"
 --
--- main :: IO ()
--- main = do
---   let port = 80
---   putStrLn (F.format "+ Listening on port {}" [port])
---   run port (slash jpgto)
---    return ()
--- @
+-- > main :: IO ()
+-- > main = let port = 3000 in putStrLn ("+ Listening on port " <> show port) >> run port (slashSimple jpgto)
 --
 -- For the full example (since this one is missing a ton of imports),
 -- see the @examples/@ directory on GitHub.
@@ -64,6 +43,7 @@ module Network.Linklater
        (
          say,
          slash,
+         slashSimple,
          Channel(..),
          User(..),
          Message(..),
@@ -77,16 +57,17 @@ import Data.Aeson
 import Data.ByteString.Lazy as BSL
 import qualified Data.Map as M
 import Data.Text.Lazy (Text)
-import qualified Data.Text.Lazy.Encoding as TLE
 import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TLE
+import Network.HTTP.Types (status200)
 import qualified Network.Wai as W
-import Network.Wreq hiding (params)
+import Network.Wreq hiding (params, headers)
 
--- | Where slash commands can come from, and where messages can go.
+-- | Where 'slash' commands come from, and where 'Message's go.
 data Channel =
    -- | A public or private group.
     GroupChannel Text
-  -- | A private conversation with your best friend (or lover?).
+  -- | A private conversation with your best friend -- or lover ;).
   | IMChannel Text
   deriving (Eq, Ord, Show)
 
@@ -104,11 +85,15 @@ data Command = Command {
   _commandText :: Maybe Text
   } deriving (Eq, Ord, Show)
 
--- | For example, @":stars2:"@. Future versions should support actual
--- images, I GUESS.
-newtype Icon = EmojiIcon Text deriving (Eq, Ord, Show)
+-- | The icon next to the messages you `say`. (Images unsupported
+-- right now, sorry.)
+newtype Icon =
+  -- | For example, ":stars2:".
+  EmojiIcon Text deriving (Eq, Ord, Show)
 
--- | Here's how you talk.
+-- | Here's how you talk: you make one of these and pass it to 'say'.
+-- Before the day is done, Linklater will convert this to a JSON blob
+-- using 'Data.Aeson'.
 data Message = Message {
   -- | The icon you want your message to appear as.
   _messageIcon :: Icon,
@@ -116,9 +101,8 @@ data Message = Message {
   -- Alert: if it's a private IM, it'll look like it came from
   -- slackbot (as of writing).
   _messageChannel :: Channel,
-  -- | What you want to say. This will be parsed in full (parse=full).
-  -- In the future I'll support other forms of parsing, hopefully.
-  -- Poke me with a pull request if I forget.
+  -- | Your big ideas go here. This will be parsed in full
+  -- (parse=full) in the Slack API sense.
   _messageText :: Text
   } deriving (Eq, Ord, Show)
 
@@ -134,16 +118,19 @@ instance ToJSON Message where
       stringOfChannel (GroupChannel c) = TL.concat ["#", c]
       stringOfChannel (IMChannel m) = TL.concat ["@", m]
 
--- | Like a curiosity about the world, you'll need one of these to say something.
+-- | Like a curiosity about the world, you'll need one of these to
+-- 'say' something.
 data Config = Config {
-  -- | This is where your Slack account is hosted. For example, 'trello.slack.com'.
+  -- | This is where your Slack account is hosted. For example,
+  -- @"trello.slack.com"@.
   _configHostname :: Text,
-  -- | This is the incoming web hook token that Slack gave you. It's usually a long alphanumberic string of garbage.
+  -- | This is the incoming web hook token that Slack gave you. It's
+  -- usually a long alphanumberic string of garbage.
   _configIncomingHookToken :: Text
   }
 
--- | The 'say' function posts a Message, with a capital M, to Slack.
--- It'll, ahem, need your token first though.
+-- | The 'say' function posts a 'Message', with a capital M, to Slack.
+-- It'll, however, need a 'Config' (a.k.a. incoming token) first.
 say :: Message -> Config -> IO (Response ByteString)
 say message config =
   post (TL.unpack url) (encode message)
@@ -151,8 +138,21 @@ say message config =
     -- TODO: use a URL builder
     url = TL.concat ["https://", _configHostname config, "/services/hooks/incoming-webhook?token=", _configIncomingHookToken config]
 
--- | A bot server! As if by magic. This acts like a WAI middleware in
--- that you let us wrap around your application.
+-- | A bot server for people who are in a hurry. Make a function that
+-- takes a 'Command' and returns some 'Text' in 'IO' world, and we'll
+-- convert it into a 'Network.WAI' application. If you want more
+-- control over the request and respond, see 'slash'.
+slashSimple :: (Maybe Command -> IO Text) -> W.Application
+slashSimple f =
+  slash (\command _ respond -> f command >>= (respond . makeResponse))
+  where
+    headers = [("Content-type", "text/plain")]
+    makeResponse = W.responseLBS status200 headers . TLE.encodeUtf8
+
+-- | A bot server! As if by magic. This acts like a 'Network.WAI'
+-- middleware: Linklater wraps around your application. (Really, it
+-- just gives you a 'Command' to work with instead of a raw HTTP
+-- request.)
 slash :: (Maybe Command -> W.Application) -> W.Application
 slash f req respond = f command req respond
   where
