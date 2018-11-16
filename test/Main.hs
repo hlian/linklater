5698fa972233733c1b1010ffcc21fa0acd27c5fc
Dom De Re <domdere@domdere.com>
2018-11-16 23:56:47 +1100

    tests

--- test/Main.hs
+++ test/Main.hs
@@ -4,6 +4,10 @@ module Main where
 
 import qualified Data.Map as Map
 
+import Data.Bifunctor (bimap)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import Network.HTTP.Base (urlEncodeVars)
 import Network.Linklater.Types
 
 import Test.Tasty
@@ -11,11 +15,11 @@ import Test.Tasty.HUnit
 
 commandOfParamsTest :: Assertion
 commandOfParamsTest =
-  commandOfParams incoming @?= fixture
+  commandOfParams fullBody incoming @?= fixture
   where
     fixture =
-      Right (Command "prove" (User "Knuth") (Channel "C2147483705" "test") (Just "text"))
-    incoming = Map.fromList [
+      Right (Command "prove" (User "Knuth") (Channel "C2147483705" "test") (Just "text") "https://hooks.slack.com/commands/00/B4R" "token=XXXXXXXXXXXXXXXXXX&team_id=T0001&team_domain=example&channel_id=C2147483705&channel_name=test&timestamp=1355517523.000005&user_id=U2147483697&user_name=Knuth&text=text&command=%2Fprove&response_url=https%253A%252F%252Fhooks.slack.com%252Fcommands%252F00%252FB4R")
+    vars = [
         ("token", "XXXXXXXXXXXXXXXXXX")
       , ("team_id", "T0001")
       , ("team_domain", "example")
@@ -26,7 +30,14 @@ commandOfParamsTest =
       , ("user_name", "Knuth")
       , ("text", "text")
       , ("command", "/prove")
+      , ("response_url", "https%3A%2F%2Fhooks.slack.com%2Fcommands%2F00%2FB4R")
       ]
+    fullBody = T.encodeUtf8
+      . T.pack
+      . urlEncodeVars
+      . fmap (bimap T.unpack T.unpack)
+      $ vars
+    incoming = Map.fromList vars
 
 unitTests :: TestTree
 unitTests =
